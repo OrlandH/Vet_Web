91b3e1225ba98220f149eec70addaa7ab907dca9
OrlandH <24heyerunsu@gmail.com>
2024-01-22 16:23:07 -0500

    Clase

---
+++ src/controllers/tratamiento_controller.js
@@ -0,0 +1,28 @@
+import Tratamiento from "../models/Tratamiento.js"
+import mongoose from "mongoose";
+const detalleTratamiento = (req,res)=>{
+    res.send("Detalle del tratamiento")
+}
+const registrarTratamiento = async (req,res)=>{
+    const {paciente} = req.body
+    if( !mongoose.Types.ObjectId.isValid(paciente) ) return res.status(404).json({msg:`Lo sentimos, debe ser un id válido`});
+    const tratamiento = await Tratamiento.create(req.body)
+    res.status(200).json({msg:`Registro exitoso del tratamiento ${tratamiento._id}`,tratamiento})
+}
+const actualizarTratamiento = (req,res)=>{
+    res.send("Actualizar tratamiento")
+}
+const eliminarTratamiento = (req,res)=>{
+    res.send("Eliminar tratamiento")
+}
+const cambiarEstado = (req,res)=>{
+    res.send("Cambiar estado del tratamiento")
+}
+
+export {
+    detalleTratamiento,
+    registrarTratamiento,
+    actualizarTratamiento,
+    eliminarTratamiento,
+    cambiarEstado
+}
--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import routerVeterinarios from "./routers/veterinario_routes.js";
+import routerPacientes from './routers/paciente_routes.js';
+import routerTratamiento from './routers/tratamiento_routes.js';
 // Crear una instancia de express
 //Inicializaciones
 const app = express();
@@ -18,7 +20,9 @@ app.use(express.json())
 
 
 // Rutas 
+app.use('/api',routerPacientes);
 app.use("/api", routerVeterinarios);
+app.use("/api", routerTratamiento);
 // Manejo de una ruta que no sea encontrada
 app.use((req,res)=>res.status(404).send("Endpoint no encontrado - 404"))
 // Exportar la instancia de express por medio de app
